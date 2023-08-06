56cb46c053bd473ec4e301fd28eb46cfd0f2c962
madhouse <35741027+m4dhouse@users.noreply.github.com>
2023-08-07 01:03:31 +0200

    Update V.3.1
    * Fixed MMVpn
    * Removed MMAnimated
    * Fixed VideoEnhancementSetup
    * Moved all screens to skin_panel2.xml except Infobar - SecondInfobar - ChannelSelection

--- MadMax-Impossible-Skin/usr/lib/enigma2/python/Components/Converter/MMVpn.py
+++ MadMax-Impossible-Skin/usr/lib/enigma2/python/Components/Converter/MMVpn.py
@@ -1,34 +1,36 @@
-import os
-import re
+from os import path, popen, listdir
 from Components.Converter.Converter import Converter
 from Components.Element import cached
 
-class MMVpn(Converter, object):
-    VPNLOAD = 0
+class MMVpn(Converter):
+	VPNLOAD = 0
 
-    def __init__(self, type):
-        Converter.__init__(self, type)
-        if type == 'vpn':
-            self.type = self.VPNLOAD
+	def __init__(self, type):
+		Converter.__init__(self, type)
+		if type == 'vpn':
+			self.type = self.VPNLOAD
 
-    @cached
-    def getBoolean(self):
-        info = False
-        if os.path.isfile("/etc/openvpn/openvpn.log"):
-            read_log = open("/etc/openvpn/openvpn.log", "r")
-            for line in read_log:
-                if re.search("Initialization Sequence Completed", line):
-                    info = True
-                elif re.search("VERIFY ERROR", line):
-                    info = False
-                elif re.search("TLS Error: TLS handshake failed", line):
-                    info = False
-                elif re.search("AUTH: Received control message: AUTH_FAILED", line):
-                    info = False
+	@cached
+	def getBoolean(self):
+		tun_vpn = False
+		if "openvpn" in str(listdir("/var/run")):
+			try:
+				check_tun = popen("ip tuntap show").read().split()[0]
+				if 'tun0:' in check_tun:
+					tun_vpn = True
+			except IndexError:
+				pass
+		if path.exists("/var/run/resolvconf/interfaces"):
+			if "wg0" in str(listdir("/var/run/resolvconf/interfaces")):
+				try:
+					check_wireguard = popen("wg show").read().split()
+					if "handshake:" in check_wireguard:
+						tun_vpn = True
+				except IndexError:
+					pass
+		return tun_vpn
 
-        return info
+	boolean = property(getBoolean)
 
-    boolean = property(getBoolean)
-
-    def changed(self, what):
-        Converter.changed(self, what)
+	def changed(self, what):
+		Converter.changed(self, what)
